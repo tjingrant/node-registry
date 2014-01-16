c9ac904797d658d71e087f194f7f4a6b4107d578
tjin <tjingrant@gmail.com>
2014-01-16 23:56:17 +0800

    get param method added

--- node-registry.js
+++ node-registry.js
@@ -1,9 +1,24 @@
+var _ = require('underscore');
+var assert = require('assert');
+
 module.exports = exports = (function() {
   'use strict';
 
   return function() {
 
     var _dict = {};
+    var _dep = {};
+
+    function getParam(func) {
+
+      var fn = func.toString()
+                   .replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg, '');
+      var params = fn.slice(fn.indexOf('(') + 1,
+                            fn.indexOf(')'))
+                     .match(/([^\s,]+)/g);
+      return (params || []);
+
+    }
 
     this.set = function(name, func) {
       _dict = func;
@@ -12,6 +27,18 @@ module.exports = exports = (function() {
     this.get = function(name) {
       return _dict[name];
     };
+
+    this.invoke = function(name, op, args, dep) {
+
+      if (op.di) {
+        assert.equal({}, args);
+        var merged = _merge(_dep, dep);
+
+      } else {
+        _dict[name].apply(null, args);
+      }
+
+    }
 
     this.update = function(name, func) {
       _dict[name] = func;
